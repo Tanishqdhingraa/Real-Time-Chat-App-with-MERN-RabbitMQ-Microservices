6633dd62aa514c1a7d2a353c4e49f57a82e88d67
Tanishq dhingra <dhingratanishq58@gmail.com>
2025-12-13 18:58:44 +0530

    working on chats

---
+++ Backend/chat/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from "dotenv";
+
+const app = express()
+
+dotenv.config();
+
+const port = process.env.PORT || 8001; 
+
+app.listen(port,()=>{
+    console.log(`Chat services is running on http://localhost:${port} `);
+    
+})
